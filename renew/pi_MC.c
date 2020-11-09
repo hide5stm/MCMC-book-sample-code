d88e5fc9d3a94455e8105c0b2735441001c09628
Hideaki Igarashi <hide+github@5stm.net>
2020-11-09 22:45:51 +0900

    add Makefile

--- renew/pi_MC.c
+++ renew/pi_MC.c
@@ -2,20 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(void){
-  int niter=1000000;
-  srand((unsigned)time(NULL)); 
+int main(void) {
+    int niter = 1000000;
+    srand((unsigned)time(NULL));
 
-  int n_in=0;
-  /*************/
-  /* Main loop */
-  /*************/
-  for(int iter=1;iter<niter+1;iter++){
+    int n_in = 0;
+    /*************/
+    /* Main loop */
+    /*************/
+    for (int iter = 1; iter < niter + 1; iter++) {
+        double x = (double)rand() / RAND_MAX;
+        double y = (double)rand() / RAND_MAX;
 
-    double x = (double)rand()/RAND_MAX;
-    double y = (double)rand()/RAND_MAX;
-
-    if(x*x+y*y < 1e0)
-      n_in=n_in+1;
-    printf("%d   %.10f\n",iter,(double)n_in/iter);}
+        if (x * x + y * y < 1e0) {
+            n_in = n_in + 1;
+        }
+        printf("%d   %.10f\n", iter, (double)n_in / iter);
+    }
 }
